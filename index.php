7911670a576f116f99a2a2523e21f619afb09aaa
Dorcsi49 <76173855+Dorcsi49@users.noreply.github.com>
2021-05-01 11:08:48 +0200

    wwww
    ww

--- index.php
+++ index.php
@@ -4,15 +4,9 @@
 <?php include "php/menu.php" ?>
 
     <div class="container">
-              <div class="header">
-        <body>
-    <title>Carlo Lovarda</title>
-    <div class="container">
         <div class="row">
-            <div class="col">
-                <div class="box">
-                    <div class="box-content">
-                        <b>
+            <div class="col-12 col-md-6">
+                      <b>
                             A Carlo Lovassport Egyesület 2012ben alakult ráckevei székhellyel.
                             Célunk egy olyan lovas közösség összekovácsolása ahol mindenki kortól és nemtől függetlenül élvezheti a lovak és a természet közeli élet előnyeit.
                             Fő profilunk az alapos lovas oktatás ezen belül pedig az utánpótlás nevelés. Képzett pónikkal és lovakkal dolgozunk, így tanítványainkat az alapoktól kezdve akár a versenyszerű lovaglásig tudjuk kísérni. Lovasaink számos versenyen eredményességükkel bizonyították hogy a kemény munkánknak igenis beérik a gyümölcse.
@@ -30,24 +24,29 @@
                             A Tanár úr! Aki ismerte és volt szerencséje vele együtt dolgozni, csak így emlegette. Nem volt egy egyszerű eset, megkövetelte helyes lovaglás irányelveit. Nagyon sok embert tanított meg lovagolni.
                             Életének utolsó pár évében kehességgel küzdött majd 2011.augusztus 18-án egy terep alkalmával örökre elvágtázott.
                         </b>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="box">
-                    <div class="box-content">
+        </div>
+            <div class="col-12 col-md-6">
+                <div class="row">
+                    <div class="col-6">
                         <img class="img-thumbnail" src="img/lovardakép.jpg" width="240" height="240"
-                             style="float: right" alt="Carlo Lovarda" title="Carlo Lovarda">
-                        <img class="img-thumbnail" src="img/lovarda2.jpg" width="240" height="240"
-                             style="float: right" alt="Carlo Lovarda" title="Carlo Lovarda">                        
-                        <img class="img-thumbnail" src="img/lovarda3.jpg" width="240" height="240"
+                             style="float: right" alt="Carlo Lovarda" title="Carlo Lovarda">                                     
+                        
+                             </div>
+                <div class="col-6">
+                <img class="img-thumbnail" src="img/lovarda2.jpg" width="240" height="240"
+                             style="float: right" alt="Carlo Lovarda" title="Carlo Lovarda">                  
+                    </div>
+                <div class="col-6">
+                <img class="img-thumbnail" src="img/lovarda3.jpg" width="240" height="240"
                              style="float: left" alt="Carlo Lovarda" title="Carlo Lovarda">
-                        <img class="img-thumbnail" src="img/carlo2.jpg" width="240" height="240"
+                    </div>
+                <div class="col-6">
+                <img class="img-thumbnail" src="img/carlo2.jpg" width="240" height="240"
                              style="float: left" alt="Carlo Lovarda" title="Carlo Lovarda">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</body>
+
 <?php include 'php/footer.php'; ?>
